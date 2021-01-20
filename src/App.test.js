5c1e7086e777c4e00410037481640c956ad3a487
Andrew McManus <60327690+andrewemcmanus@users.noreply.github.com>
2021-01-20 10:53:09 -0500

    Initial commit

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import Welcome from './components/Welcome';
+
+test('render the Welcome header', () => {
+  render(<Welcome />);
+  const h1Element = screen.getByText(/Welcome/i);
+  expect(h1Element).toBeInTheDocument();
+});
