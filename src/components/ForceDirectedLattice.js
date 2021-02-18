bb1ec881061a4612c7fc524d5fb148181040974b
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-02-18 08:49:02 -0500

    [forcedirectedlattice] looking at functionality of incoming data

--- src/components/ForceDirectedLattice.js
+++ src/components/ForceDirectedLattice.js
@@ -1,12 +1,59 @@
 import React, {useRef, useEffect} from "react";
 import {Runtime, Inspector} from "@observablehq/runtime";
+// import rd3 from 'd3file';
+// const RD3Component = rd3.Component;
 import notebook from "@d3/force-directed-lattice";
+import nodes from './data/nodes.json';
+
+// chart = {
+//   replay;
+//
+//   const links = data.links.map(d => Object.create(d));
+//   const nodes = data.nodes.map(d => Object.create(d));
+//
+//   const simulation = d3.forceSimulation(nodes)
+//       .force("charge", d3.forceManyBody().strength(-30))
+//       .force("link", d3.forceLink(links).strength(1).distance(20).iterations(10))
+//       .on("tick", ticked);
+//
+//   invalidation.then(() => simulation.stop());
+//
+//   const context = DOM.context2d(width, height);
+//
+//   function ticked() {
+//     context.clearRect(0, 0, width, height);
+//     context.save();
+//     context.translate(width / 2, height / 2);
+//     context.beginPath();
+//     for (const d of links) {
+//       context.moveTo(d.source.x, d.source.y);
+//       context.lineTo(d.target.x, d.target.y);
+//     }
+//     context.strokeStyle = "#aaa";
+//     context.stroke();
+//     context.beginPath();
+//     for (const d of nodes) {
+//       context.moveTo(d.x + 3, d.y);
+//       context.arc(d.x, d.y, 3, 0, 2 * Math.PI);
+//     }
+//     context.fill();
+//     context.strokeStyle = "#fff";
+//     context.stroke();
+//     context.restore();
+//   }
+//
+//   return d3.select(context.canvas)
+//       .call(drag(simulation)
+//       .subject(({x, y}) => simulation.find(x - width / 2, y - height / 2)))
+//     .node();
+// }
+
 
 function ForceDirectedLattice() {
   const ref = useRef();
 
   useEffect(() => {
-    console.log(notebook);
+    console.log(nodes);
     (new Runtime).module(notebook, name => {
       if (name === "d3") return Inspector.into(ref.current.querySelector(".d3"))();
       return ["chart","drag"].includes(name);
